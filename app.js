fd37b21abba84732330278e75304f96523a6d148
JoLeaper <joleap123@gmail.com>
2020-03-25 15:46:54 -0700

    created 5 objects, stored objects in array

---
+++ app.js
@@ -0,0 +1,8 @@
+const jotaroKujo = {userName: 'Jotaro Kujo', standName: 'Star Platinum', standType: 'Close-Range'};
+const josephJoestar = {userName: 'Joseph Joestar', standName: 'Hermit Purple', standType: 'Mid-Range'};
+const mohammadAvdol = {userName: 'Mohammad Avdol', standName: 'Magician\'s Red', standType: 'Long-Range'};
+const kakyoinNoriyaki = {userName: 'Kakyoin Noriyaki', standName: 'Hierophant Green', standType: 'Long-Range'};
+const jeanPierrePolnareff = {userName: 'Jean Pierre Polnareff', standName: 'Silver Chariot', standName: 'Close-Range'};
+const stardustCrusaders = [jotaroKujo, josephJoestar, mohammadAvdol, kakyoinNoriyaki, jeanPierrePolnareff];
+
+console.log(stardustCrusaders);
