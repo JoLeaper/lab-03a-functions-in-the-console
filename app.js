55ee609f20897252e30b22bed66130e4636c19e1
JoLeaper <joleap123@gmail.com>
2020-03-25 16:52:18 -0700

    added switch statement and categorized

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-const jotaroKujo = {userName: 'Jotaro Kujo', standName: 'Star Platinum', standType: 'Close-Range'};
-const josephJoestar = {userName: 'Joseph Joestar', standName: 'Hermit Purple', standType: 'Mid-Range'};
-const mohammadAvdol = {userName: 'Mohammad Avdol', standName: 'Magician\'s Red', standType: 'Long-Range'};
-const kakyoinNoriyaki = {userName: 'Kakyoin Noriyaki', standName: 'Hierophant Green', standType: 'Long-Range'};
-const jeanPierrePolnareff = {userName: 'Jean Pierre Polnareff', standName: 'Silver Chariot', standType: 'Close-Range'};
+const jotaroKujo = {userName: 'Jotaro Kujo', standName: 'Star Platinum', standRange: 'C'};
+const josephJoestar = {userName: 'Joseph Joestar', standName: 'Hermit Purple', standRange: 'D'};
+const mohammadAvdol = {userName: 'Mohammad Avdol', standName: 'Magician\'s Red', standRange: 'C'};
+const kakyoinNoriyaki = {userName: 'Kakyoin Noriyaki', standName: 'Hierophant Green', standRange: 'A'};
+const jeanPierrePolnareff = {userName: 'Jean Pierre Polnareff', standName: 'Silver Chariot', standRange: 'C'};
 const stardustCrusaders = [jotaroKujo, josephJoestar, mohammadAvdol, kakyoinNoriyaki, jeanPierrePolnareff];
 
 console.log(stardustCrusaders);
 
 const logsOutThings = (array) => {
-    console.log('Here are your ' + array.length + ' things!');
+    console.log('Here are your ' + array.length + ' stands, categorized by their effective range!');
+    let closeRange = [];
+    let midRange = [];
+    let longRange = [];
     for(let i = 0; i < array.length; i++){
-        console.log(array[i].standType);
-        console.log(array[i]);
-    }
+        switch(array[i].standRange) {
+            case 'D':
+            case 'E':
+            closeRange.push(array[i]);
+            break;
+
+            case 'C':  
+            case 'B':
+            midRange.push(array[i]);
+            break;
+
+            case 'A':
+            longRange.push(array[i]);
+            break;
+        }
+
+    }        
+        console.log('Close-Range:');
+        console.log(closeRange);
+        console.log('Mid-Range:')
+        console.log(midRange);
+        console.log('Long-Rage');
+        console.log(longRange);
 }
 
 logsOutThings(stardustCrusaders);
