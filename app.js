f535f1b90ea391d293f99d25dfa83c1a51ac4836
JoLeaper <joleap123@gmail.com>
2020-03-25 16:00:41 -0700

    added logsOutThings function

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const jotaroKujo = {userName: 'Jotaro Kujo', standName: 'Star Platinum', standTy
 const josephJoestar = {userName: 'Joseph Joestar', standName: 'Hermit Purple', standType: 'Mid-Range'};
 const mohammadAvdol = {userName: 'Mohammad Avdol', standName: 'Magician\'s Red', standType: 'Long-Range'};
 const kakyoinNoriyaki = {userName: 'Kakyoin Noriyaki', standName: 'Hierophant Green', standType: 'Long-Range'};
-const jeanPierrePolnareff = {userName: 'Jean Pierre Polnareff', standName: 'Silver Chariot', standName: 'Close-Range'};
+const jeanPierrePolnareff = {userName: 'Jean Pierre Polnareff', standName: 'Silver Chariot', standType: 'Close-Range'};
 const stardustCrusaders = [jotaroKujo, josephJoestar, mohammadAvdol, kakyoinNoriyaki, jeanPierrePolnareff];
 
 console.log(stardustCrusaders);
+
+const logsOutThings = (array) => {
+    console.log('Here are your ' + array.length + ' things!');
+    for(let i = 0; i < array.length; i++){
+        console.log(array[i]);
+    }
+}
+
+logsOutThings(stardustCrusaders);
