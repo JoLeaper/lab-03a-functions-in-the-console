b17276a66e15b50cfafc47defd95cf8889bccffc
JoLeaper <joleap123@gmail.com>
2020-03-26 09:55:53 -0700

    added missing semi-colon, added space between beginning and ending curly braces

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const jotaroKujo = {userName: 'Jotaro Kujo', standName: 'Star Platinum', standRange: 'C'};
-const josephJoestar = {userName: 'Joseph Joestar', standName: 'Hermit Purple', standRange: 'D'};
-const mohammadAvdol = {userName: 'Mohammad Avdol', standName: 'Magician\'s Red', standRange: 'C'};
-const kakyoinNoriyaki = {userName: 'Kakyoin Noriyaki', standName: 'Hierophant Green', standRange: 'A'};
-const jeanPierrePolnareff = {userName: 'Jean Pierre Polnareff', standName: 'Silver Chariot', standRange: 'C'};
+const jotaroKujo = { userName: 'Jotaro Kujo', standName: 'Star Platinum', standRange: 'C' };
+const josephJoestar = { userName: 'Joseph Joestar', standName: 'Hermit Purple', standRange: 'D' };
+const mohammadAvdol = { userName: 'Mohammad Avdol', standName: 'Magician\'s Red', standRange: 'C' };
+const kakyoinNoriyaki = { userName: 'Kakyoin Noriyaki', standName: 'Hierophant Green', standRange: 'A' };
+const jeanPierrePolnareff = { userName: 'Jean Pierre Polnareff', standName: 'Silver Chariot', standRange: 'C' };
 const stardustCrusaders = [jotaroKujo, josephJoestar, mohammadAvdol, kakyoinNoriyaki, jeanPierrePolnareff];
 
 console.log(stardustCrusaders);
@@ -32,10 +32,10 @@ const logsOutThings = (array) => {
     }        
         console.log('Close-Range:');
         console.log(closeRange);
-        console.log('Mid-Range:')
+        console.log('Mid-Range:');
         console.log(midRange);
         console.log('Long-Rage');
         console.log(longRange);
-}
+};
 
 logsOutThings(stardustCrusaders);
